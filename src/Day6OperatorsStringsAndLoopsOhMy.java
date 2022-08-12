9e2f3ba5fc54b2f473306d7fd78346a32c2fb9b4
Iskender Colak <iclk84@gmail.com>
2022-08-12 11:16:06 +0300

    Day6 task is completed

--- src/Day6OperatorsStringsAndLoopsOhMy.java
+++ src/Day6OperatorsStringsAndLoopsOhMy.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Day6OperatorsStringsAndLoopsOhMy {
 
     public static void main(String[] args) {
 
         // Day 6 class is created
+
+        Scanner scanner = new Scanner(System.in);
+        int countOfWords = scanner.nextInt();
+
+        String strEven = "";
+        String strOdd = "";
+
+        for (int i = 0; i < countOfWords; i++) {
+            String str = scanner.next();
+            for (int j = 0; j < str.length(); j++) {
+                if (j % 2 == 0) {
+                    strEven += str.charAt(j);
+                } else {
+                    strOdd += str.charAt(j);
+                }
+            }
+            System.out.println(strEven + " " + strOdd);
+            strEven = "";
+            strOdd = "";
+        }
+        scanner.close();
     }
 }
